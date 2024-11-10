4bb514f217b486412c38e7f8a3e009fe90d6c5d4
aniketsuvaidym <aniket.suvaidyam@gmail.com>
2024-11-10 14:34:14 +0000

    add

--- components/CheckBox.vue
+++ components/CheckBox.vue
@@ -1,30 +1,28 @@
 <template>
     <div class="flex items-center space-x-2">
-        <input :id="props.label.toLowerCase()"
-            type="checkbox"
-            class="h-3 w-3 text-primary border border-[#002C77] rounded-sm"
-            :checked="props.checked"
-            @change="props.toggle"
-        />
-        <label :for="props.label.toLowerCase()" class="text-sm text-[#002C77]">{{ props.label }}</label>
+      <input 
+        :id="field.name"
+        :checked="modelValue"
+        @change="$emit('update:modelValue', $event.target.checked)"
+        type="checkbox"
+        class="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500 dark:border-gray-600 dark:focus:ring-blue-600"
+        :disabled="field.read_only"
+      />
+      <label :for="field.name" class="text-sm text-gray-700 dark:text-gray-200">{{ field.label }}</label>
     </div>
-</template>
-
-<script setup>
-import { ref } from 'vue'
-
-const props = defineProps({
-    label: {
-        type: String,
-        required: true,
+  </template>
+  
+  <script setup>
+  defineProps({
+    field: {
+      type: Object,
+      required: true
     },
-    checked: {
-        type: Boolean,
-        required: true,
-    },
-    toggle: {
-        type: Function,
-        required: false,
-    },
-})
-</script>
+    modelValue: {
+      type: Boolean,
+      required: true
+    }
+  })
+  
+  defineEmits(['update:modelValue'])
+  </script>
