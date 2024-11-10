82125ae6097c0bcd0ef80079286dc818e71f720e
rsuvaidyam <rkrahul00011@gmail.com>
2024-11-10 07:04:15 +0000

    add

--- FormView.vue
+++ FormView.vue
@@ -1,11 +1,39 @@
 <template>
-    message:    {{state.message}}   
-    <Button></Button>
+  <Button /> 
+  <div class="p-2">
+  <Input />
+  <CheckBox :checked="false" label="Funder Diagnostic"/>
+</div>
 </template>
 <script setup>
-  import { reactive, ref } from 'vue'
-  import {Button} from './components/index'
-  const state = reactive({
-      message:"Hello World",
-  })
+import { inject, onMounted, ref, watch } from 'vue'
+import { Button,Input ,CheckBox} from './components'
+
+
+const call = inject('$call')
+const fields = ref([])
+const props = defineProps({
+  doctype: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: false,
+  },
+  showFormButton: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+})
+const getMeta = async () => {
+  const res = await call('pwit.controllers.api.get_meta',
+    { doctype: props.doctype }
+  )
+  fields.value = res.fields
+}
+onMounted(() => {
+  getMeta()
+})
 </script>
--- components/index.js
+++ components/index.js
@@ -1,1 +1,3 @@
 export {default as Button} from './Button.vue';
+export {default as Input} from './Input.vue';
+export {default as CheckBox} from './CheckBox.vue';
