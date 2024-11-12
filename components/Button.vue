4bd75a3be4ef4b53096aed515598ddbac3265464
aniketsuvaidym <aniket.suvaidyam@gmail.com>
2024-11-12 08:24:08 +0000

    add

--- components/Button.vue
+++ components/Button.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     :disabled="field.read_only"
-    class="w-full px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
+    class="w-full px-4 py-2 bg-secondary text-white rounded-md  focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
     @click="$emit('click')"
   >
     {{ field.label }}
